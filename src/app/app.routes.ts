1837bc12f33be140f13f56234c755f91f28512cd
JoaquinFerman <joacoferman@gmail.com>
2025-08-22 11:43:01 -0300

    initial push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        loadComponent: () =>
+            import('./pages/bienvenida/bienvenida').
+        then((archivo) => archivo.Bienvenida)
+    },
+    {
+        path : 'bienvenida',
+        loadComponent: () =>
+            import('./pages/bienvenida/bienvenida').
+        then((archivo) => archivo.Bienvenida)
+    },
+    {
+        path : 'error',
+        loadComponent: () =>
+            import('./pages/error/error').
+        then((archivo) => archivo.Error)
+    },
+    {
+        path : 'login',
+        loadComponent: () =>
+            import('./pages/login/login').
+        then((archivo) => archivo.Login)
+    },
+    {
+        path : 'registro',
+        loadComponent: () =>
+            import('./pages/registro/registro').
+        then((archivo) => archivo.Registro)
+    },
+    {
+        path : 'sobre-mi',
+        loadComponent: () =>
+            import('./pages/sobre-mi/sobre-mi').
+        then((archivo) => archivo.SobreMi)
+    },
+    {
+        path : '**',
+        redirectTo : 'error'
+    }
+];
